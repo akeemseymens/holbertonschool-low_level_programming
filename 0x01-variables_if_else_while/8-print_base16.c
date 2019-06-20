d3d2ec5f10784099c50ecc5c7a1c662c2a605a37
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-20 23:17:27 +0000

    adding 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+/**
+ * main - Entry point
+ * Description -print alphabet and go past e and q.
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a';c < 'g'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
