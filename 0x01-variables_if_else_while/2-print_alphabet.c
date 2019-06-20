37de7380c26fa04532a927dd784e19633fc747ee
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-20 21:21:36 +0000

    added 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description -print alphabet in lowercase; followed by new line.
+ * Return: Always 0 (Success)
+ */
+
+int main ()
+{
+	char ch;
+
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar(\n);
+
+	return(0);
+}
