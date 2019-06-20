8c911f743b86061ef40e1d260da09f303636170b
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-20 22:19:29 +0000

    added 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ * Description -print alphabet and go past e and q.
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 10 ; n++)
+	{
+		printf(n
+	}
+
+	putchar('\n');
+
+	return (0);
+}
