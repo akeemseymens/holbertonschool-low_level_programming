88f7da4e95cd8cdfb165318a552fea94606f5b95
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-20 21:51:49 +0000

    spaces deleted

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
 /**
  * main - Entry point
  * Description -print alphabet in lowercase; followed by new line.
  * Return: Always 0 (Success)
  */
+#include <stdio.h>
 
-int main (void)
+int main(void)
 {
 	int ch;
 
