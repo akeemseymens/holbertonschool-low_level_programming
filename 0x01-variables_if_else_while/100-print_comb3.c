19688029e06dd3cbe606e14b1b27ba2329fc37d3
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-21 01:47:29 +0000

    added 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+/**
+ * main - Entry point
+ * Description -print the numbers from 00 to 99
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9' ; i++)
+	{
+		for (j = i + 1 ; j <= '9' ; j++);
+		{
+			putchar(i);
+			putchar(j);
+			if (!(i == '8' && j == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
