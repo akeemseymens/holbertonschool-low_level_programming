f41c025745890e0e59f053bb6d9811dce310e848
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-25 22:43:32 +0000

    rewriting comments.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- *is_alpha - check the code.
- *
+ * _isalpha - check the code.
+ *@c: int
  *Return: Always 0.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
