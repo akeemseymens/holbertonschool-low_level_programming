d14475001250df46203495b28fce671b98c95630
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-26 00:19:04 +0000

    for function syntax error.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * jack_bauer entry - Write a function that -
+ * prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * @i @j @n : int
+ *Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int n = i * j;
+
+			if (n < 10)
+			{
+				if (j)
+					_putchar(' ');
+				_putchar('0' + n);
+			}
+			else
+			{
+				_putchar('0' + (n / 10));
+				_putchar('0' + (n % 10));
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
