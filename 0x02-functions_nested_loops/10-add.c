ae4146d63234bd1ee148d0a9eee0c3a64d7a65ef
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-26 00:40:28 +0000

    adding 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - addition function.
+ * @a, @b : integers.
+ * description - addition of number.
+ *Return: addition of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
