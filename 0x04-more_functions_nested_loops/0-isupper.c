71bf503a96abe4d42769c9741cc0ddb3fbe0a24e
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-27 19:26:53 +0000

    description format correction.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 /**
  * _isupper - checks for uppercase characters.
- *@c - is an int.
- * Return: Always 0.
+ * @c: is an int.
+ * Return: Always 0 for lower and 1 for lower.
  */
 int _isupper(int c)
 {
 
-	if (c >= 'A' && c<= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
-	}else
-		return(0);
+		return (1);
+
+	}
+	else
+		return (0);
 }
