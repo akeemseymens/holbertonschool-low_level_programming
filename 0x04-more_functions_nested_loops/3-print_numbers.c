c6dca38a616f9676bfbd5377972889417eb90d67
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-27 19:19:38 +0000

    adding file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints numbers 0 to 9, followed by new line.
+ * @i - int parameter.
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for(i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
