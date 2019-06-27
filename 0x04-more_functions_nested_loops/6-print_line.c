953115e1b55df9d8f170eb1d7f214b3f3a048f41
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-27 21:54:38 +0000

    added 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_line -  draws a straight line in the terminal.
+ * @n: int range parameter.
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
