83c3c191962c6de98baa64e38af6638831d00843
akeemseymens <akeem.seymens@holbertonschool.com>
2019-06-28 00:48:36 +0000

    added 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - do the fizz buzz.
+ *
+ * Description: Fizz Buzz.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("50829599");
+	return (0);
+}
