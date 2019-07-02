b746e52b8c870209665047c3680425ee198877a7
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-02 21:43:47 +0000

    added file 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_rev -   function that prints one char out of 2 of a string + a new line.
+ * @s: character pointer.
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; x < _strlen(str); str += 2)
+	{
+		_putchar(str[x]);
+	}
+
+	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	return (p - s);
+}
