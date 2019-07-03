1c6f4195a9c445346b2b0c2fa998aa29d22bae5f
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-03 00:00:10 +0000

    added 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
- * print_array - prints n elements of an array of integer & new line.
+ * _strcpy - prints n elements of an array of integer & new line.
  * @dest: char
  * @src: char string.
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i = 0;
 
-	while ((*dest++ = *src++) != '\0')
-		;
+	while (*src != '\0')
+	{
+		*(dest + i) = *(src);
+		i++;
+		src++;
+	}
+	*(dest + i) = '\0';
 	return (dest);
 }
