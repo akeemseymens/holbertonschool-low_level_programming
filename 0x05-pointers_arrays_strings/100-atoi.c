656c780f3e0b3844f534462786127f0c057e274e
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-03 00:51:44 +0000

    added 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_array - prints n elements of an array of integer & new line.
+ * @s: char
+ *
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	int res = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	for(; s[i] != '\0'; ++i)
+		res = res*10 + s[i] - '0';
+
+	return sign*res;
+}
