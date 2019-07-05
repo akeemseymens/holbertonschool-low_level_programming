f70bf0a7bf59fd46f0cc8fd0ff7a74462585a6d7
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-05 21:26:09 +0000

    added number 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strcmp - Write a function that compares two strings.
+ * @s1: char pointer.
+ * @s2: char pointe
+ * Return: Always dest.
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	for ( ; *s1 == *s2; s1++, s2++)
+	{
+		if (*s1 == '\0')
+			return (0);
+	}
+	return (*s1 - *s2);
+}
