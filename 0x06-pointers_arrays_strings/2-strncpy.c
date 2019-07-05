4d2c45ed361403f015692e380b57c34974b92980
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-05 21:25:36 +0000

    added 1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcat - Write a function that copy a string.
+ * @dest: char pointer.
+ * @src: char pointer
+ * @n: integer.
+ * Return: Always dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *tmp = dest;
+	int i;
+
+	for (i = 0; i < n &&  *src != '\0'; i++)
+		*tmp++ = *src++;
+	for (; i < n; i++)
+		*tmp++ = '\0';
+
+	return (dest);
+}
