19d4c670689cd00bc21ffe0e287dfe31198881c0
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-09 19:24:41 +0000

    added first attempt.

---
+++ 0x07-pointers_arrays_Strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memset - A function that fills memory with constant byte.
+ * @s: char pointer
+ * @b: char pointer
+ * @n: unsigned int. 
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	while (--n)
+	{
+		*s++ = b;
+	}
+
+	return (s);
+}
