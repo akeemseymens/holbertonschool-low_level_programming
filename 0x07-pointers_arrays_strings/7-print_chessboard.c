214b4eef08997dac315bcf4e292f0f9e72ee00f0
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-10 02:42:59 +0000

    added 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* print_chessboard - print the chessboard
+* @a: pointer for chess board represented as an 8x8 array
+*
+* Return: void
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; ++row)
+	{
+		for (col = 0; col < 8; ++col)
+			_putchar(a[row][col]);
+		_putchar('\n');
+	}
+}
