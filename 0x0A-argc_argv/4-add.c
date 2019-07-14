223ade988c5cc731c3011ca00b2b9d689009e4c5
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-14 20:45:50 +0000

    first attempt

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - print result of addition of arguments
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 passes a positive integer, or 1 if failed.
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int sum;
+	int i;
+
+	sum = 0;
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+			if (!isdigit(argv[argc][i]))
+			{
+				puts("Error");
+				return (1);
+			}
+
+		sum += atoi(argv[argc]);
+	}
+
+	printf("%u\n", sum);
+
+	return (0);
+}
