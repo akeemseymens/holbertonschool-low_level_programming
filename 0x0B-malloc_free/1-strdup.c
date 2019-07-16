00702a1befc635dd64d7dafea2f81ffbb3c77dfd
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-16 23:16:48 +0000

    first attempt

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * _strdup - returns copy of new string in memory.
+ * @str: string pointer.
+ *
+ * Return: string.
+ */
+
+char *_strdup(char *str)
+{
+	char *dest;
+	int l, i;
+
+	l = _strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+	dest = malloc(sizeof(char) * (l + 1));
+
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; i <= l; i++)
+		dest[i] = str[i];
+	return (dest);
+
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: character pointer.
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	return (p - s);
+}
