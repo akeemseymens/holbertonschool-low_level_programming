73543091a46e9f8f785f194ec20e6a6b1c648d13
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-18 19:41:33 +0000

    added header with prototypes.

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,20 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _strlen(char *s);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
