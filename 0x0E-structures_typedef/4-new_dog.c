e777b7415494b26fcca1e08e05a28df26472b92e
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-23 01:27:21 +0000

    added file.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - make a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Return: pointer to the new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	d = malloc(sizeof(*d));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = _strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->age = age;
+	d->owner = _strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	return (d);
+
+}
+/**
+ * _strdup - duplicate a string on the heap
+ * @s: string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *s)
+{
+	char *buffer, *tmp;
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	i++;
+
+	buffer = malloc(sizeof(*buffer) * i);
+	if (buffer == NULL)
+		return (NULL);
+
+	tmp = buffer;
+
+	while ((*tmp++ = *s++))
+		;
+
+	return (buffer);
+}
