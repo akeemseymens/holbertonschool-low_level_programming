733f51e26cfae5384c1a43d01c155b9db1f7b8dc
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-23 00:26:09 +0000

    added the h file.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+void _putchar(char);
+
+/**
+ * struct dog - dog description
+ * @name: name of dog
+ * @age: age of dog
+ *@owner: name of owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* #ifndef _DOG_H */
