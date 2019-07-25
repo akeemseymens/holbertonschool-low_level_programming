557387ff7431150ca15cb2c33d0418a2a2f60cc8
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-25 17:56:41 +0000

    reformated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,9 +22,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i])
+	while (ops[i].op)
 	{
-		if (!strcmp(ops[i].op, s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
