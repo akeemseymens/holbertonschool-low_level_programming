341146f1f4e46322ce19ecc58709af956e5ad37c
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-25 17:29:41 +0000

    fixed bug

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,12 +15,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 		exit(98);
 
-	if (atio(argv[3]) == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
-	{
-		print("Error\n");
-		exit(100);
-	}
-	printf("%d\n", (*get_op_func)(argv[2])(atio(argv[1]), atio(argv[3])));
+	printf("%d\n", (get_op_func)(argv[2])(atio(argv[1]), atio(argv[3])));
 
 	return (0);
 }
