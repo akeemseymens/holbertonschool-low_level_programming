7535a8247a5ea63ee9bd5abd2da6e9dedc52f1e4
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-25 04:28:14 +0000

    first attempt.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+
+#include <stdlib.h>
+
+/**
+ * print_name - print a name
+ * @name: name to print
+ * @f: printer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
