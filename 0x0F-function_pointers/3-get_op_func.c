7ccd7796c5d239c5872527601336502b8135e881
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-25 17:05:55 +0000

    changed syntax

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,10 +18,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+
 	int i;
 
 	i = 0;
-	while (i < 5)
+	while (ops[i])
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
