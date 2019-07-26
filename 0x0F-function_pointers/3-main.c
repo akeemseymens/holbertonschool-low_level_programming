c1f5119fd79497b53c622ae05ce897188bab4ee1
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-26 22:45:58 +0000

    added edge case.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,13 +10,27 @@
 
 int main(int argc, char *argv[])
 {
+	int num1, num2, (*fn)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	printf("%d\n", (*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3])));
-
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		puts("Error");
+		return (100);
+	}
+	fn = get_op_func(argv[2]);
+	if (fn == NULL)
+	{
+		puts("Error");
+		return (99);
+	}
+	printf("%d\n", fn(num1, num2));
 	return (0);
 }
