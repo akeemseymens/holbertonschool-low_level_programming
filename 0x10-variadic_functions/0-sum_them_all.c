1519819019a92e0d2b9ff2da4ffc01be07e33ef8
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-28 02:42:18 +0000

    added formating.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
+#include <strarg.h>
 
 /**
  * sum_them_all - returns the summ of all its parameters.
- * @n: unsigned integer variant.
+ * @n: const unsigned integer variant.
  * Return: Always total (SUCESS)
  */
 
@@ -10,14 +11,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list sum;
 	unsigned int i;
-	int total = 0;
+	double total = 0.0;
 
 	if (n == 0)
 		return (0);
+
 	va_start(sum, n);
 
 	for (i = 0; i < n; i++)
-		total += va_arg(sum, int);
+		total = total + va_arg(sum, int);
+
 	va_end(sum);
+
 	return (total);
 }
