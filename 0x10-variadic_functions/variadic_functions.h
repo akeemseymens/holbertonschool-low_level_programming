471dbed9c193118473a9e1b9f8a422bad5774488
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-28 19:10:44 +0000

    added comments on header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,9 +13,9 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 /**
- *
- *
- *
+ *struct ops - struct of functions and flags
+ *@ic: pointers to char string
+ *@ch: pointer to list
  *
  */
 typedef struct ops
