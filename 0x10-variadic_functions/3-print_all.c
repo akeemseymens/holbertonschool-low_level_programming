085e7563af9f09c37f32c3b9141c42dac0da1c5f
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-29 00:39:44 +0000

    made betty compliant

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,8 +3,8 @@
 #include <stdarg.h>
 
 /**
- * get_op_func - convert symbol into function
- * @s: string which may correspoind to function
+ * op_char - convert symbol into function
+ * @list: string which may correspoind to function
  *
  * Return: function associated with s
  */
@@ -13,8 +13,8 @@ void op_char(va_list list)
 	printf("%c", va_arg(list,  int));
 }
 /**
- * get_op_func - convert symbol into function
- * @s: string which may correspoind to function
+ * op_int - convert symbol into function
+ * @list: string which may correspoind to function
  *
  * Return: function associated with s
  */
@@ -23,8 +23,8 @@ void op_int(va_list list)
 	printf("%d", va_arg(list, int));
 }
 /**
- * get_op_func - convert symbol into function
- * @s: string which may correspoind to function
+ * op_float - convert symbol into function
+ * @list: string which may correspoind to function
  *
  * Return: function associated with s
  */
@@ -33,8 +33,8 @@ void op_float(va_list list)
 	printf("%f", va_arg(list, double));
 }
 /**
- * get_op_func - convert symbol into function
- * @s: string which may correspoind to function
+ * op_char_ptr - convert symbol into function
+ * @list: string which may correspoind to function
  *
  * Return: function associated with s
  */
@@ -48,8 +48,8 @@ void op_char_ptr(va_list list)
 	printf("%s", str);
 }
 /**
- * get_op_func - convert symbol into function
- * @s: string which may correspoind to function
+ * print_all - convert symbol into function
+ * @format: pointer char.
  *
  * Return: function associated with s
  */
@@ -64,10 +64,10 @@ void print_all(const char * const format, ...)
 	va_start(list, format);
 
 	op_t f_ops[] = {
-		{"c" , op_char},
-		{"i" , op_int},
-		{"f" , op_float},
-		{"s" , op_char_ptr},
+		{"c", op_char},
+		{"i", op_int},
+		{"f", op_float},
+		{"s", op_char_ptr},
 		{NULL, NULL}
 	};
 	i = 0;
@@ -78,7 +78,7 @@ void print_all(const char * const format, ...)
 		{
 			/*printf("%d j",j);*/
 
-			if (*f_ops[j].c == format[j])
+			if (*f_ops[j].c == format[i])
 			{
 				printf("%s", separator);
 				f_ops[j].ch(list);
