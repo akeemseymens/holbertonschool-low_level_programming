ee6ba1a31d8a30b2c3c1111f5ee4ca74dbfd11e3
akeemseymens <812@holbertonschool.com>
2019-08-01 21:48:33 +0000

    betty compliant

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *print_list
+ *print_list - Write a function that prints all the elements of a list_t list.
  *@h: list ptr list
- *
+ *Return: Count of print array.
  */
 
 size_t print_list(const list_t *h)
@@ -10,13 +10,13 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		if(h -> str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n",  h->len, h->str);
-		h = h -> next;
+		h = h->next;
 		count++;
 	}
 	return (count);
