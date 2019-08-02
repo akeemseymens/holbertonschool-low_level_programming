d2c18ddd21ea7054fc3d6f848df48bfc025fd5d2
akeemseymens <812@holbertonschool.com>
2019-08-02 00:27:13 +0000

    added file.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *free_list - Function that frees a list
+ *@head: Pointer
+ *Return: size_t counter value
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp  = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
