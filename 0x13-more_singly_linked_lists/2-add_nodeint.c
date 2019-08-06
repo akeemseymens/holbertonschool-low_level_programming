5448ba2e975fa086cca753f3527b5fc9e91f58ca
akeemseymens <812@holbertonschool.com>
2019-08-06 17:22:12 +0000

    added file.

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *add_node - a function that returns the number of elements linked list_t list.
+ *@head: list ptr list
+ *@str: content in the list.
+ *Return: structure type
+ */
+
+listint_t *add_nodeint(list_t **head, const int *n)
+{
+	list_t *new_node;
+
+	*new_node = malloc(sizeof(*new_node));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (head);
+}
