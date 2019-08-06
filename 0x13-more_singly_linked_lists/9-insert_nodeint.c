d06ac5cbf276a52cfa809dc30b23820063162642
akeemseymens <812@holbertonschool.com>
2019-08-06 23:05:16 +0000

    logic format.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
 
-	if (!head && idx)
+	if (!head && !idx)
 		return (NULL);
 	if (idx && *head)
 		return (insert_nodeint_at_index(&(*head)->next, idx - 1, n));
