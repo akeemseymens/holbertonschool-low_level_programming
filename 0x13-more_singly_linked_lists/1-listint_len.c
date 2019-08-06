ddf17a2415941dac9d736ea22f1180263de578f4
akeemseymens <812@holbertonschool.com>
2019-08-06 17:12:23 +0000

    added file.

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *listint_len - a function that returns the number of elements linked list_t list.
+ *@h: list ptr list
+ *Return: Count of print array.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
