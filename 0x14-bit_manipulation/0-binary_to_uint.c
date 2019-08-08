14da16de00f464ac4d5ecefc832b65191d95f4c1
akeemseymens <812@holbertonschool.com>
2019-08-08 20:51:11 +0000

    betty compliant and return int not char.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - a function that converts a binary number to an unsigned int.
- *
+ * binary_to_uint - a function that converts/
+ * a binary number to an unsigned int.
+ *@b: binary string.
  * Return: the convertted number, or 0 if the char is not 0 or 1 or Null for b.
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
+
 	if (!b)
-		return ('0');
+		return (0);
 
 	while (*b)
 	{
@@ -19,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 			n += 1;
 		else if (*b != '0')
 			return (0);
-		b++;
+		++b;
 	}
 	return (n);
 }
