56435182223b50aba20312c867cf481ded6cda2e
akeemseymens <812@holbertonschool.com>
2019-08-08 20:46:28 +0000

    added file.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - a function that converts a binary number to an unsigned int.
+ *
+ * Return: the convertted number, or 0 if the char is not 0 or 1 or Null for b.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	if (!b)
+		return ('0');
+
+	while (*b)
+	{
+		n <<= 1;
+
+		if (*b == '1')
+			n += 1;
+		else if (*b != '0')
+			return (0);
+		b++;
+	}
+	return (n);
+}
