459fab47c12f3d04f60bfa466e3e59bc3a9e5cc0
akeemseymens <812@holbertonschool.com>
2019-08-09 00:30:49 +0000

    betty compliance.

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- * get_bit - check the code for Holberton School students.
+ *set_bit -  a function that sets the
+ *value of a bit to 1 at a given index.
  *@n: number being evaluated.
  *@index: number of interating.
- * Return: Always 0.
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
