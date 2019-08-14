1addc67a317cd30b73eaf09f040ea97f85599a4e
akeemseymens <812@holbertonschool.com>
2019-08-14 00:19:40 +0000

    variable mistype

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (1);
 
-	for (i = 0; text_count[i] != '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
 	wr = write(fd, text_content, i);
