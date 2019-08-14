002a1ed12a5bcdc9a9a42632ce2d7af3a0155e75
akeemseymens <812@holbertonschool.com>
2019-08-14 06:46:26 +0000

    added 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_org , fd_cp, rd = 1, wr;
+	char buff[1024];
+
+	function_count(argc);
+
+	fd_org = open(argv[1], O_RDONLY);
+	if (fd_org == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from
+file %s\n", argv[1]);
+		exit(98); }
+	fd_cp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_cp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
+	while (rd != 0)
+	{
+		rd = read(fd_from, buf, 1024);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from
+file %s\n", argv[1]);
+			exit(98); }
+		if (rd != 0)
+		{
+			wr = write(fd_cp, buff, r);
+			if (wr == -1)
+			{dprintf(STDERR_FILENO, "Error: Can't write
+to %s\n", argv[2]);
+				exit(99);
+			}
+		}
+	}
+	function_close(fd_org);
+	function_close(fd_cp);
+	return (0);
+}
+
+/**
+ * function_count - test the failure count.
+ * @ac: testing int var.
+ * Return: Always 0.
+ */
+void function_count(int ac)
+{
+	if (ac != 3)
+        {
+                dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+                exit(97);
+        }
+}
+
+/**
+ * function_close - closes and test the close.
+ * @fd: function descriptor.
+ * Return: Always 0.
+ */
+
+void function_close(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+	if (cl == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd),
+                        exit(100);
+	}
+}
+
+void function_count(int ac);
+void function_close(int fd);
