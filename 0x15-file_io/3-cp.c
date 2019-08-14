b6eb0501ed3701c68bfd80d48b0267702d44aa52
akeemseymens <812@holbertonschool.com>
2019-08-14 07:08:52 +0000

    fixed compiled errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,28 +18,28 @@ int main(int argc, char *argv[])
 	fd_org = open(argv[1], O_RDONLY);
 	if (fd_org == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from
-file %s\n", argv[1]);
-		exit(98); }
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd_cp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_cp == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99); }
+		exit(99);
+	}
 	while (rd != 0)
 	{
-		rd = read(fd_from, buf, 1024);
+		rd = read(fd_org, buff, 1024);
 		if (rd == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from
-file %s\n", argv[1]);
-			exit(98); }
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		if (rd != 0)
 		{
-			wr = write(fd_cp, buff, r);
+			wr = write(fd_cp, buff, rd);
 			if (wr == -1)
-			{dprintf(STDERR_FILENO, "Error: Can't write
-to %s\n", argv[2]);
+			{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 				exit(99);
 			}
 		}
