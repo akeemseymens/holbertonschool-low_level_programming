b1fa528ac8268a8b5b8d3d156f9e34f33c2e28de
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-16 05:20:04 +0000

    betty compliant

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * print_dlistin - Prints all the elements of the list.
- *
- *Returns the count.
+ * print_dlistint - Prints all the elements of the list.
+ * @h: header and list.
+ * Return: the count.
  **/
 size_t print_dlistint(const dlistint_t *h)
 {
