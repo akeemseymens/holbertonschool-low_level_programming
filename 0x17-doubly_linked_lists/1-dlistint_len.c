23a20bf118db0dc71e8e035a0b52de763029549b
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-16 05:22:16 +0000

    betty formatted

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 /**
  * dlistint_len - Prints all the elements of the list.
  * @h: pointer linked list.
- * Returns the count.
+ * Return: the count.
  **/
 size_t dlistint_len(const dlistint_t *h)
 {
