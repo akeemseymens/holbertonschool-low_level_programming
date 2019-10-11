de9457522cfd60b574e3ba73c55761fd400d0d16
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-11 20:42:29 +0000

    added file 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - add an element to the hash table
+ *@ht: hash table to add element to hash
+ *@key: key of element
+ *@value: value of the element
+ *Return: 1 if SUCCESS else 0 for FAILURE
+ **/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, **head, *tmp;
+
+	index = key_index((const unsigned char *) key, ht->size);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+
+	}
+	new_node = malloc(sizeof(hash_node_t *));
+	if (!new_node)
+		return (0);
+	head = &ht->array[index];
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = *head;
+	*head = new_node;
+	return (1);
+
+}
