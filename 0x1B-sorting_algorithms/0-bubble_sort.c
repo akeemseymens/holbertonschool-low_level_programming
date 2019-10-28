6e7f7600414aefb79c755873cd7daa3b653cec2c
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-28 23:50:56 +0000

    betty

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,32 +1,31 @@
 #include "sort.h"
 
 /**
- * bubble_sort -
- *@array - array that is being sorted
- *@size - size of the array.
+ * bubble_sort - uses bubble algorithms
+ * @array: array that is being sorted
+ * @size: size of the array.
  * Return: printed sort array until completion.
  */
 void bubble_sort(int *array, size_t size)
 {
 	int flag;
-	unsigned i, tmp;
+	unsigned int i, tmp;
 
 	if (!array)
 		return;
 
-	do
-	{
+	do {
 		flag = 0;
 		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i] > array[i+1])
+			if (array[i] > array[i + 1])
 			{
-				tmp = array[i+1];
-				array[i+1] = array[i];
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
 				array[i] = tmp;
 				flag = 1;
 				print_array(array, size);
 			}
 		}
-	}while (flag);
+	} while (flag);
 }
