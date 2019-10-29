beca04e1b953f293f49d9a83d456e971ff640ade
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-29 19:45:20 +0000

    edge addition

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -10,6 +10,9 @@ void selection_sort(int *array, size_t size)
 {
 	unsigned int i, j, min_idx, temp;
 
+	if (!array)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
@@ -18,9 +21,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min_idx])
 			min_idx = j;
 		}
-		temp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = temp;
-		print_array(array, size);
+		if (array[i] != array[min_idx])
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
 	}
 }
