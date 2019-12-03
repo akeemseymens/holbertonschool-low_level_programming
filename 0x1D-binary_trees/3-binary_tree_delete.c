46d2b90037b73691a9f93d540340d1911c57864d
akeemseymens <812@holbertonschool.com>
2019-12-02 19:14:19 -0800

    added files

---
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/*
+* binary_tree_delete - function that deletes an entire binary tree
+* @tree: is a pointer to the root node of the tree to delete
+* Returns - nothing.
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	if (tree->left)
+		binary_tree_delete(tree);
+	if (tree->right)
+		binary_tree_delete(tree);
+	free(tree);
+}
