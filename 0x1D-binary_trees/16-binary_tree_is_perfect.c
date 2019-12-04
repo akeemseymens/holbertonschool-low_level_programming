7d5a2b99bc366a77643ccd6cdcd4284816134635
akeemseymens <812@holbertonschool.com>
2019-12-04 23:49:41 +0000

    betty compliant.

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -10,8 +10,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-    if (binary_tree_height(tree->left) !=
-	    binary_tree_height(tree->right))
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
