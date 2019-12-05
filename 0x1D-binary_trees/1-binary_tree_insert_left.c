c88bc0829dd9039efcf6061125c53d9e6db27199
akeemseymens <812@holbertonschool.com>
2019-12-04 17:01:07 -0800

    betty compliant

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
-/*
-* binary_tree_insert_right -  inserts a node as the left-child of another node.
-* @parent - Pointer to the parent node.
-* @value - value of the node
-* Returns: pointer to the created node, or NULL on failure or if parent is NULL
-*/
+/**
+*binary_tree_insert_left -  inserts a node as the left-child of another node.
+*@parent: Pointer to the parent node.
+*@value: value of the node
+* Return: pointer to the created node, or NULL on failure or if parent is NULL
+**/
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
