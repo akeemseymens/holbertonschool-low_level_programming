d2de89b10b26b68657635b47712a6193d46bccbc
akeemseymens <812@holbertonschool.com>
2019-12-05 13:44:23 -0800

    changed edge case

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -11,8 +11,8 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (!tree)
 		return;
 	if (tree->left)
-		binary_tree_delete(tree);
+		binary_tree_delete(tree->left);
 	if (tree->right)
-		binary_tree_delete(tree);
+		binary_tree_delete(tree->right);
 	free(tree);
 }
